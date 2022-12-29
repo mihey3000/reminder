2169189c70c607c43466e54f6f1b5daf02c4c613
mihey3000 <mihey3000@yandex.ru>
2022-12-29 23:25:56 +0300

    add vue & token

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -11,6 +11,10 @@ import axios from 'axios';
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.token = document.head.querySelector('meta[name="csrf-token"]');
+window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
+window.axios.defaults.headers.common['Accept'] = 'application/json';
+console.log({ axios: window.axios })
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
---
+++ resources/js/pages/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>Напоминалка111</h1>
+</template>
+
+<script>
+  export default {};
+</script>
+
+<style>
+</style>
